3b36e6be6d81f027816c5bb15e56610bdaf2707b
Tharuth Attanayake <tharuth17@gmail.com>
2023-04-04 01:18:43 -0400

    Home implemented + Flask resources
    As title.

--- Original Initial Setup/static/js/script.js
+++ Original Initial Setup/static/js/script.js
@@ -1,8 +1,157 @@
-function deleteNote(noteId) {
-  fetch("/delete-note", {
-    method: "POST",
-    body: JSON.stringify({ noteId: noteId }),
-  }).then((_res) => {
-    window.location.href = "/";
-  });
+//search bar toggle for smaller resolutions
+searchForm = document.querySelector('.search-form');
+document.querySelector('#search-btn').onclick = () =>{
+  searchForm.classList.toggle('active');
 }
+
+//login form toggle
+loginForm =document.querySelector('.login-form-container');
+document.querySelector('#login-btn').onclick = () =>{
+  loginForm.classList.toggle('active');
+}
+document.querySelector('#close-login-btn').onclick = () =>{
+  loginForm.classList.toggle('active');
+}
+
+//header deselector? whatever it's called to leave it behind
+window.onscroll = () =>{
+  searchForm.classList.remove('active');
+  if(window.scrollY > 80){
+    document.querySelector('.header .header-2').classList.add('active');
+  }else{
+    document.querySelector('.header .header-2').classList.remove('active');
+  }
+}
+
+//same as above
+window.onload = () =>{
+  if(window.scrollY > 80){
+    document.querySelector('.header .header-2').classList.add('active');
+  }else{
+    document.querySelector('.header .header-2').classList.remove('active');
+  }
+}
+
+//slidey amotion for the home tab
+var swiper = new Swiper(".books-slider", {
+  loop:true,
+  centeredSlides:true,
+  autoplay: {
+    delay: 6000,
+    disableOnInteraction:false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 1,
+    },
+    1024: {
+      slidesPerView: 1,
+    },
+  },
+});
+
+//slidey amotion for the featured tab (books of the week)
+var swiper = new Swiper(".featured-slider", {
+  spaceBetween: 10,
+  loop:true,
+  centeredSlides:true,
+  autoplay: {
+    delay: 6000,
+    disableOnInteraction:false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    450: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+//slidey animations for new books (arrivals tab)
+var swiper = new Swiper(".arrivals-slider", {
+  spaceBetween: 10,
+  loop:true,
+  centeredSlides:true,
+  autoplay: {
+    delay: 6000,
+    disableOnInteraction:false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+//slidey animation for latest reviews (potentially even all reviews?)
+var swiper = new Swiper(".reviews-slider", {
+  spaceBetween: 10,
+  grabCursor:true,
+  loop:true,
+  centeredSlides:true,
+  autoplay: {
+    delay: 6000,
+    disableOnInteraction:false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+//slidey amotion for the other books of the week
+var swiper = new Swiper(".other-slider", {
+  spaceBetween: 10,
+  loop:true,
+  centeredSlides:true,
+  autoplay: {
+    delay: 6000,
+    disableOnInteraction:false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    450: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1024: {
+      slidesPerView: 4,
+    },
+  },
+});
