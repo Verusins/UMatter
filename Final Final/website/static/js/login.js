758557fc5a85b7f048d643f6890404be2372b77b
Sig <verusins.cn@gmail.com>
2023-04-06 14:25:31 -0400

    Database setting and waiting for sign-up page to be fixed
    need some help in login.js to make sign-up page appear when trying to register

--- Final Final/website/static/js/login.js
+++ Final Final/website/static/js/login.js
@@ -6,3 +6,12 @@ document.querySelector('#login-btn').onclick = () =>{
 document.querySelector('#close-login-btn').onclick = () =>{
   loginForm.classList.toggle('active');
 }
+
+//sign-up form toggle
+sign_upForm = document.querySelector('.sign-up-form-container');
+document.querySelector('#sign-up-btn').onclick = () =>{
+  sign_upForm.classList.toggle('active');
+}
+document.querySelector('#close-sign-up-btn').onclick = () =>{
+  sign_upForm.classList.toggle('active');
+}
